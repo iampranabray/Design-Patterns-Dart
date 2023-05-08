3dd5a9945d0d738edd0f6412228cc27e8f69fa35
iampranabray <pranabray04@gmail.com>
2023-05-09 00:59:38 +0400

    Comment Added

--- Singleton.dart
+++ Singleton.dart
@@ -1,24 +1,22 @@
-void main(){
-  Singleton singleton = Singleton.getInstance(value:"Singleton Instance 1");
+void main() {
+  Singleton singleton = Singleton.getInstance(value: "Singleton Instance 1");
   print(singleton.value);
-  Singleton singleton1 = Singleton.getInstance(value:"Singleton Instance 2");
+  Singleton singleton1 = Singleton.getInstance(value: "Singleton Instance 2");
   print(singleton1.value);
 }
 
-class Singleton{
+//Singleton Class
+class Singleton {
   String? value;
-   static Singleton? instance;
-  
+  static Singleton? instance;
+
   Singleton({this.value});
-  
-  static Singleton  getInstance({String? value}){
-    if(instance==null){
-       instance = Singleton(value:value);
+
+  static Singleton getInstance({String? value}) {
+    if (instance == null) {
+      instance = Singleton(value: value);
       return instance!;
     }
     return instance!;
-    
-    
   }
-  
 }
